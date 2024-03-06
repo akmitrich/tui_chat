9c7628a66161486fbf982da784e43ad6628614d6
Alexander Kalashnikov <akalashnikov@optimalcity.ru>
2024-03-06 16:23:45 +0300

    connectors

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -3,29 +3,32 @@ use std::sync::mpsc;
 use tokio::runtime::Runtime;
 
 use crate::{
-    connector::{connector, ConnectorEvent},
+    connector::{input_connector, output_connector, ConnectorEvent},
     controller_signals::ControllerSignal,
     ui::Ui,
 };
 
 pub struct App {
     ui: Ui,
+    tx: mpsc::Sender<ControllerSignal>,
     rx: mpsc::Receiver<ControllerSignal>,
     async_runtime: Runtime,
-    async_tx: mpsc::Sender<ConnectorEvent>,
+    async_tx: tokio::sync::mpsc::Sender<ConnectorEvent>,
+    async_rx: Option<tokio::sync::mpsc::Receiver<ConnectorEvent>>,
 }
 
 impl App {
     pub fn new() -> Self {
         let (tx, rx) = mpsc::channel();
-        let (async_tx, async_rx) = mpsc::channel();
+        let (async_tx, async_rx) = tokio::sync::mpsc::channel(1024);
         let async_runtime = Runtime::new().expect("Failed to start asynchronous runtime.");
-        async_runtime.spawn(connector(async_rx, tx.clone()));
         Self {
-            ui: Ui::new(tx),
+            ui: Ui::new(tx.clone()),
+            tx,
             rx,
             async_runtime,
             async_tx,
+            async_rx: Some(async_rx),
         }
     }
 
@@ -37,6 +40,14 @@ impl App {
 
 impl App {
     fn run(mut self) {
+        if let Some(async_rx) = self.async_rx.take() {
+            self.async_runtime
+                .handle()
+                .spawn(output_connector(async_rx));
+        }
+        self.async_runtime
+            .handle()
+            .spawn(input_connector(self.tx.clone()));
         loop {
             self.process_signals();
             self.ui.step_next();
@@ -45,7 +56,7 @@ impl App {
             }
         }
         self.async_runtime
-            .shutdown_timeout(std::time::Duration::from_millis(1000));
+            .shutdown_timeout(std::time::Duration::from_millis(300));
     }
 
     fn process_signals(&mut self) {
@@ -53,10 +64,14 @@ impl App {
             match signal {
                 ControllerSignal::Submit => self.ui.submit(),
                 ControllerSignal::IncomingMessage { from, message } => {
+                    eprintln!("{:?} -> {}", from, message);
                     self.ui.append(&from, &message)
                 }
                 ControllerSignal::OutgoingMessage { message } => {
-                    let _ = self.async_tx.send(ConnectorEvent::Post { message });
+                    let tx = self.async_tx.clone();
+                    self.async_runtime.handle().spawn(async move {
+                        let _ = tx.send(ConnectorEvent::Post { message }).await;
+                    });
                 }
                 ControllerSignal::Info { message } => self.ui.present_info(&message),
                 ControllerSignal::Quit => self.ui.stop(),
@@ -65,5 +80,11 @@ impl App {
         if let Err(mpsc::TryRecvError::Disconnected) = self.rx.try_recv() {
             eprintln!("Application crashed!");
         }
+    }
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self::new()
     }
 }
--- src/connector.rs
+++ src/connector.rs
@@ -1,23 +1,61 @@
 use std::sync::mpsc;
 
+use redis::AsyncCommands;
+
 use crate::controller_signals::ControllerSignal;
 
 pub enum ConnectorEvent {
     Post { message: String },
 }
 
-pub async fn connector(rx: mpsc::Receiver<ConnectorEvent>, tx: mpsc::Sender<ControllerSignal>) {
+pub async fn output_connector(mut rx: tokio::sync::mpsc::Receiver<ConnectorEvent>) {
+    eprintln!("Output thread begins.");
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut con = client.get_async_connection().await.unwrap();
+    eprintln!("Start output");
+    while let Some(event) = rx.recv().await {
+        match event {
+            ConnectorEvent::Post { message } => {
+                let _: () = con
+                    .xadd("42", "*", &[("Master", message.as_str())])
+                    .await
+                    .unwrap();
+            }
+        }
+    }
+}
+
+pub async fn input_connector(tx: mpsc::Sender<ControllerSignal>) {
+    eprintln!("Input thread begins.");
+    let client = redis::Client::open("redis://127.0.0.1/")
+        .map_err(|e| eprintln!("Failed open client: {:?}", e))
+        .unwrap();
+    let mut con = client
+        .get_tokio_connection()
+        .await
+        .map_err(|e| eprintln!("Failed get connection: {:?}", e))
+        .unwrap();
+    eprintln!("Start input");
     loop {
-        match rx.recv() {
-            Ok(event) => match event {
-                ConnectorEvent::Post { message } => {
-                    let _ = tx.send(ControllerSignal::IncomingMessage {
-                        from: "Me".to_owned(),
-                        message,
-                    });
-                }
-            },
-            Err(_e) => break,
+        let opts = redis::streams::StreamReadOptions::default()
+            .count(1)
+            .block(100);
+        let result: Result<redis::streams::StreamReadReply, _> =
+            con.xread_options(&[42], &["$"], &opts).await;
+        match result {
+            Ok(result) if !result.keys.is_empty() => {
+                let _ = tx.send(ControllerSignal::IncomingMessage {
+                    from: "Redis".to_owned(),
+                    message: format!("{:?}", result),
+                });
+                eprintln!("{:?}", result);
+            }
+            Err(e) => {
+                let _ = tx.send(ControllerSignal::Info {
+                    message: format!("ERROR: {:?}", e),
+                });
+            }
+            _ => {}
         }
     }
 }
